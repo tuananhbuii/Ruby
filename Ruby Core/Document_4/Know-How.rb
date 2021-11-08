8d7df3cf7e1a426ed2caff56f253fe2729dd002c
Bùi Tuấn Anh <56882009+tuananhbuii@users.noreply.github.com>
2021-11-08 10:55:00 +0700

    Commit

---
+++ Ruby Core/Document_4/Know-How.rb
@@ -0,0 +1,15 @@
+
+puts "Enter some text: "
+text = gets.chomp
+
+puts "Enter words to redact: "
+redact = gets.chomp
+
+words = text.split(" ")
+
+words.each { |word|
+  if word == redact
+    print "REDACTED "
+  else
+    print word + " "
+  end }
---
+++ Ruby Core/Document_4/Redacted.rb
@@ -0,0 +1,9 @@
+
+puts "Enter some text: "
+text = gets.chomp
+
+puts "Enter words to redact: "
+redact = gets.chomp
+
+words = text.split(" ")
+words.each { |word| print word }
